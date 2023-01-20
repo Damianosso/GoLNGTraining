3f7b6c5ebc5a7c1ecf87ce9c65018a9582d52a51
Damianosso <123141207+Damianosso@users.noreply.github.com>
2023-01-20 04:24:02 +0000

    Update main.go

--- 02_package/main/main.go
+++ 02_package/main/main.go
@@ -7,8 +7,12 @@ import (
 	//someAlias "github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
 )
 
+
 func main() {
 	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 	fmt.Println(stringutil.MyName)
 	fmt.Println(winniepooh.BearName)
 }
+
+
+// RETURN
